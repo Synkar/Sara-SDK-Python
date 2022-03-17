3b921a68437327ba6f84b4566e282b0cc99195f2
Rennan Freitas <rennan.elit@gmail.com>
2022-03-17 15:25:46 -0300

    feat(rest): add attach and detach functions to rest functions

--- sara_sdk/utils/rest.py
+++ sara_sdk/utils/rest.py
@@ -60,7 +60,7 @@ def create(resource, payload, session=None, **kwargs):
         Json: The json of the content of the response sended by the api
 
     Example:
-        >>> create(resource="iam/robots", payload=Dict)
+        >>> create(resource="iam/robots", **kwargs)
     """
     json = fetch(method=post, path=resource, session=session,
                  query=kwargs, payload=payload).json()
@@ -104,9 +104,74 @@ def update(resource, id, session=None, **payload):
         Json: The json of the content of the response sended by the api
 
     Example:
-        >>> delete(resource="iam/robots", "09594aae-7e88-4c8b-b4e5-095c6e785509", payload=Dict)
+        >>> delete(resource="iam/robots", "09594aae-7e88-4c8b-b4e5-095c6e785509", **payload)
     """
     path = "{endpoint}/{id}".format(endpoint=resource, id=id)
     json = fetch(method=patch, path=path,
                  session=session, payload=payload).json()
+    return json
+
+
+def attach(resource, type, this, that, session=None):
+    """
+    Attach function to attach something (this) to other entity (that)
+
+    Args:
+        resource (string): the route to access on the api
+        type (string): type of entity that is going to be attach
+        this (string): an uuid from the thing that is going to be attach to the entity
+        that (string): uuid from the entity
+        session (Session): session where to get the authorization (only needed if doens't want
+        to use the DEFAULT_SESSION)
+
+    Returns:
+        Json: The json of the content of the response sended by the api
+
+    Example:
+        >>> attach("iam/groups", "user", "9871a24a-89ff-4ba1-a350-458244e8244d", "00ea03f1-5b2e-4787-8aa6-745961c6d506")
+    """
+    body = {
+        type: that
+    }
+
+    # Workaround to userGroup route
+    if type == "user" and resource == "iam/groups":
+        type = "UserGroup"
+
+    path = "{endpoint}/{this}/attach{type}".format(
+        endpoint=resource, this=this, type=type.capitalize())
+    json = fetch(method=post, path=path, payload=body, session=session)
+    return json
+
+
+def detach(resource, type, this, that, session=None):
+    """
+    Attach function to detach something (this) from other entity (that)
+
+    Args:
+        resource (string): the route to access on the api
+        type (string): type of entity that is going to be detach
+        this (string): an uuid from the thing that is going to be detach from the entity
+        that (string): uuid from the entity
+        session (Session): session where to get the authorization (only needed if doens't want
+        to use the DEFAULT_SESSION)
+
+    Returns:
+        Json: The json of the content of the response sended by the api
+
+    Example:
+        >>> detach("iam/groups", "user", "9871a24a-89ff-4ba1-a350-458244e8244d", "00ea03f1-5b2e-4787-8aa6-745961c6d506")
+    """
+
+    body = {
+        type: that
+    }
+
+    # Workaround to userGroup route
+    if type == "user" and resource == "iam/groups":
+        type = "UserGroup"
+
+    path = "{endpoint}/{this}/attach{type}".format(
+        endpoint=resource, this=this, type=type.capitalize())
+    json = fetch(method=_delete, path=path, payload=body, session=session)
     return json
