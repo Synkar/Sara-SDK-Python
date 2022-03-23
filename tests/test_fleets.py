02fd8aaf0100300818a82849a65ba01230ef3460
Rennan Freitas <rennan.elit@gmail.com>
2022-03-23 12:23:39 -0300

    feat(Tests): add tests to fleets and fix error on test_robots

--- tests/test_fleets.py
+++ tests/test_fleets.py
@@ -2,7 +2,7 @@ from sara_sdk.common.session import Session
 from dotenv import load_dotenv
 import pytest
 import os
-import sara_sdk.core.iam.robots as sara_sdk
+import sara_sdk.core.iam.fleets as sara_sdk
 
 
 @pytest.fixture
@@ -16,21 +16,21 @@ def default_session():
     return new_session
 
 
-def test_listing_robots(default_session):
+def test_fleets_list(default_session):
     result = sara_sdk.list(session=default_session, page=1, page_size=10)
     assert result["count"] >= 0
 
 
-def test_retrieve_robot(default_session: Session):
+def test_fleets_retrieve(default_session):
     result = sara_sdk.retrieve(
-        uuid="f8b85a7a-4540-4d46-a2ed-00e6134ee84a", session=default_session)
-    assert result["uuid"] == "f8b85a7a-4540-4d46-a2ed-00e6134ee84a"
+        uuid="3ddc1eb5-8433-4eca-a95d-ff2d688cc2fc", session=default_session)
+    assert result["uuid"] == "3ddc1eb5-8433-4eca-a95d-ff2d688cc2fc"
 
 
-def test_update_robot(default_session: Session):
+def test_fleets_update(default_session):
     model = {
-        "name": "Test Robot"
+        "name": "Test Fleet"
     }
     result = sara_sdk.update(
-        uuid="f8b85a7a-4540-4d46-a2ed-00e6134ee84a", model=model, session=default_session)
-    assert result["name"] == "Test Robot"
+        uuid="3ddc1eb5-8433-4eca-a95d-ff2d688cc2fc", model=model, session=default_session)
+    assert result["name"] == "Test Fleet"
