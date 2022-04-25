17deb6a9bdefcd9e408563a1580470d68063132e
Rennan Freitas <rennan.elit@gmail.com>
2022-04-25 15:32:30 -0300

    fix(test_apps): fix test_apps to use requests_mock

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,10 +1,9 @@
 from sara_sdk.common.session import Session
 from dotenv import load_dotenv
+from sara_sdk import API_URL
 import pytest
 import os
 import sara_sdk.core.iam.apps as sara_sdk
-
-app_created = ""
 
 
 @pytest.fixture
@@ -18,30 +17,34 @@ def default_session():
     return new_session
 
 
-def test_apps_create(default_session):
+def test_apps_create(default_session, requests_mock):
     model = {
         "name": "test_app",
         "description": "description"
     }
+    requests_mock.post(f'{API_URL}v1/iam/apps/', status_code=201, json=model)
     result = sara_sdk.create(model=model, session=default_session)
-    global app_created
-    app_created = result["id"]
-    assert result["name"] == "test_app"
+    assert result == model
 
 
-def test_apps_list(default_session):
+def test_apps_list(default_session, requests_mock):
+    requests_mock.get(f'{API_URL}v1/iam/apps/',
+                      status_code=200, json=[{'name': 'app_name'}])
     result = sara_sdk.list(session=default_session)
-    assert len(result) > 0
+    assert result == [{'name': 'app_name'}]
 
 
-def test_apps_retrieve(default_session):
-    global app_created
+def test_apps_retrieve(default_session, requests_mock):
+    requests_mock.get(f'{API_URL}v1/iam/apps/51qoapqvprat87o7jkqd1beae9/',
+                      status_code=200, json={'name': 'app_name', 'id': '51qoapqvprat87o7jkqd1beae9'})
     result = sara_sdk.retrieve(
-        id=app_created, session=default_session)
-    assert result["id"] == app_created
+        id="51qoapqvprat87o7jkqd1beae9", session=default_session)
+    assert result["id"] == '51qoapqvprat87o7jkqd1beae9'
 
 
-def test_apps_delete(default_session):
-    global app_created
-    result = sara_sdk.delete(id=app_created, session=default_session)
-    assert result == {}
+def test_apps_delete(default_session, requests_mock):
+    requests_mock.delete(f'{API_URL}v1/iam/apps/51qoapqvprat87o7jkqd1beae9/',
+                         status_code=200, json={'name': 'app_name', 'id': '51qoapqvprat87o7jkqd1beae9'})
+    result = sara_sdk.delete(
+        id='51qoapqvprat87o7jkqd1beae9', session=default_session)
+    assert result["id"] == '51qoapqvprat87o7jkqd1beae9'
