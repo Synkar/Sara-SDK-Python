dc35feb81ff62b88087e7eb1cc01c8e19799a109
Rennan Freitas <rennan.elit@gmail.com>
2022-04-25 15:21:42 -0300

    fix(test_actions): fix tests on test_actions to use requests_mock

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,5 +1,6 @@
 from sara_sdk.common.session import Session
 from dotenv import load_dotenv
+from sara_sdk import API_URL
 import pytest
 import os
 import sara_sdk.core.iam.actions as sara_sdk
@@ -16,12 +17,24 @@ def default_session():
     return new_session
 
 
-def test_actions_list(default_session):
+def test_actions_list(default_session, requests_mock):
+    requests_mock.get(f'{API_URL}v1/iam/actions/?page=1&page_size=10',
+                      status_code=200, json=[{'name': 'action_name'}])
     result = sara_sdk.list(session=default_session, page=1, page_size=10)
-    assert len(result) > 0
+    assert result == [{'name': 'action_name'}]
 
 
-def test_actions_retrieve(default_session):
+def test_actions_retrieve(default_session, requests_mock):
+    requests_mock.get(f'{API_URL}v1/iam/actions/3e8b4190-8d34-4299-bc52-4617e0711b70/',
+                      status_code=200, json={'uuid': '3e8b4190-8d34-4299-bc52-4617e0711b70'})
     result = sara_sdk.retrieve(
         uuid="3e8b4190-8d34-4299-bc52-4617e0711b70", session=default_session)
     assert result["uuid"] == "3e8b4190-8d34-4299-bc52-4617e0711b70"
+
+
+def test_actions_create(default_session, requests_mock):
+    data = {'name': 'AttachRobot', 'type': 'A',
+            'service': '7e5ebea3-955d-45b0-8c03-8a7f9de95e37'}
+    requests_mock.post(f'{API_URL}v1/iam/actions/', status_code=201, json=data)
+    result = sara_sdk.create(data, default_session)
+    assert result == data
