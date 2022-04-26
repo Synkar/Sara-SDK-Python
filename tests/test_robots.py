1c9125767bd0ad4177b0cd6093582dfb95c7d947
Rennan Freitas <rennan.elit@gmail.com>
2022-04-26 14:21:27 -0300

    fix(test_robots): change routes of test robots to use requests mock

--- tests/test_robots.py
+++ tests/test_robots.py
@@ -1,5 +1,6 @@
 from sara_sdk.common.session import Session
 from dotenv import load_dotenv
+from sara_sdk import API_URL
 import pytest
 import os
 import sara_sdk.core.iam.robots as sara_sdk
@@ -16,21 +17,50 @@ def default_session():
     return new_session
 
 
-def test_listing_robots(default_session):
+def test_robots_listing(default_session, requests_mock):
+    data = [{
+        "uuid": "f8b85a7a-4540-4d46-a2ed-00e6134ee84a",
+        "name": "test robot",
+        "description": "description test",
+        "client": "f89356bd-294d-4f55-8395-c4825a296b95"
+    }]
+    requests_mock.get(f'{API_URL}v1/iam/robots/?page=1&page_size=10',
+                      status_code=200, json=data)
     result = sara_sdk.list(session=default_session, page=1, page_size=10)
-    assert result["count"] >= 0
+    assert result == data
 
 
-def test_retrieve_robot(default_session: Session):
+def test_robots_retrieve(default_session, requests_mock):
+    data = {
+        "uuid": "f8b85a7a-4540-4d46-a2ed-00e6134ee84a",
+        "name": "test robot",
+        "description": "description test",
+        "client": "f89356bd-294d-4f55-8395-c4825a296b95"
+    }
+    requests_mock.get(f'{API_URL}v1/iam/robots/f8b85a7a-4540-4d46-a2ed-00e6134ee84a/',
+                      status_code=200, json=data)
     result = sara_sdk.retrieve(
         uuid="f8b85a7a-4540-4d46-a2ed-00e6134ee84a", session=default_session)
-    assert result["uuid"] == "f8b85a7a-4540-4d46-a2ed-00e6134ee84a"
+    assert result == data
 
 
-def test_update_robot(default_session: Session):
+def test_robots_update(default_session, requests_mock):
     model = {
-        "name": "Test Robot"
+        "uuid": "f8b85a7a-4540-4d46-a2ed-00e6134ee84a",
+        "name": "test robot",
+        "description": "description test",
+        "client": "f89356bd-294d-4f55-8395-c4825a296b95"
     }
+    requests_mock.patch(f'{API_URL}v1/iam/robots/f8b85a7a-4540-4d46-a2ed-00e6134ee84a/',
+                        status_code=200, json=model)
     result = sara_sdk.update(
         uuid="f8b85a7a-4540-4d46-a2ed-00e6134ee84a", model=model, session=default_session)
-    assert result["name"] == "Test Robot"
+    assert result == model
+
+
+def test_robots_delete(default_session, requests_mock):
+    requests_mock.delete(f'{API_URL}v1/iam/robots/f8b85a7a-4540-4d46-a2ed-00e6134ee84a/',
+                         status_code=200, json={'detail': 'Robot deleted successfully'})
+    result = sara_sdk.delete(
+        uuid="f8b85a7a-4540-4d46-a2ed-00e6134ee84a", session=default_session)
+    assert result['detail'] == 'Robot deleted successfully'
